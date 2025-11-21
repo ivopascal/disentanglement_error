fb73956a3a2062114133111e7697052332d6dee8
Ivo de Jong <ivopascal@gmail.com>
2025-11-21 15:39:58 +0100

    Add pytorch implementations

--- disentanglement_error/error_metric.py
+++ disentanglement_error/error_metric.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from disentanglement_error.decreasing_dataset import decreasing_dataset_experiment
+from disentanglement_error.decreasing_dataset import decreasing_dataset_experiment, decreasing_dataset_experiment_torch
 from disentanglement_error.label_noise import label_noise_experiment
 from disentanglement_error.util import Config, RunResults, CustomJsonEncoder
 
@@ -19,6 +19,26 @@ def calculate_disentanglement_error(x_train, y_train, disentangling_model, x_tes
     for run in range(config.n_runs):
         decreasing_dataset_error, decreasing_dataset_result  = decreasing_dataset_experiment(x_train, y_train, x_test, y_test, disentangling_model, config)
         label_noise_error, label_noise_result = label_noise_experiment(x_train, y_train, x_test, y_test, disentangling_model, config)
+        results.label_noise_results.append(label_noise_result)
+        results.decreasing_dataset_results.append(decreasing_dataset_result)
+        disentanglement_errors.append((decreasing_dataset_error + label_noise_error) / 4)
+
+    if return_json:
+        results_json = json.dumps(results, cls=CustomJsonEncoder)
+        config_json = json.dumps(config, cls=CustomJsonEncoder)
+        return np.mean(disentanglement_errors), results_json, config_json
+    else:
+        return np.mean(disentanglement_errors)
+
+
+def calculate_disentanglement_error_torch(train_dataset, val_dataset, disentangling_model, batch_size, num_workers, kw_config=None, return_json=True):
+    config = Config(**kw_config)
+
+    disentanglement_errors = []
+    results = RunResults()
+    for run in range(config.n_runs):
+        decreasing_dataset_error, decreasing_dataset_result  = decreasing_dataset_experiment_torch(train_dataset, val_dataset, disentangling_model, config, batch_size, num_workers)
+        label_noise_error, label_noise_result = label_noise_experiment_torch(train_dataset, val_dataset, disentangling_model, config, batch_size, num_workers)
         results.label_noise_results.append(label_noise_result)
         results.decreasing_dataset_results.append(decreasing_dataset_result)
         disentanglement_errors.append((decreasing_dataset_error + label_noise_error) / 4)
--- disentanglement_error/util.py
+++ disentanglement_error/util.py
@@ -1,10 +1,16 @@
 import dataclasses
 import json
+from enum import Enum
+
+import torch.distributed as dist
+
 import pandas as pd
 from dataclasses import dataclass, field
 from typing import List
 
 import numpy as np
+import torch
+
 
 def json_results_to_df(json_results, json_config):
     result = json.loads(json_results)
@@ -48,3 +54,63 @@ class ExperimentResults:
 class RunResults:
     label_noise_results: List[ExperimentResults] = field(default_factory=lambda: [])
     decreasing_dataset_results: List[ExperimentResults] = field(default_factory=lambda: [])
+
+
+
+class Summary(Enum):
+    NONE = 0
+    AVERAGE = 1
+    SUM = 2
+    COUNT = 3
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, use_accel, fmt=':f', summary_type=Summary.AVERAGE):
+        self.name = name
+        self.use_accel = use_accel
+        self.fmt = fmt
+        self.summary_type = summary_type
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def all_reduce(self):
+        if self.use_accel:
+            device = torch.accelerator.current_accelerator()
+        else:
+            device = torch.device("cpu")
+        total = torch.tensor([self.sum, self.count], dtype=torch.float32, device=device)
+        dist.all_reduce(total, dist.ReduceOp.SUM, async_op=False)
+        self.sum, self.count = total.tolist()
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+    def summary(self):
+        fmtstr = ''
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.3f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.3f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {count:.3f}'
+        else:
+            raise ValueError('invalid summary type %r' % self.summary_type)
+
+        return fmtstr.format(**self.__dict__)
