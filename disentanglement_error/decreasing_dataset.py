clean up and fix UDE calculation

disentanglement_error/decreasing_dataset.py
@@ -30,13 +30,13 @@ def decreasing_dataset_experiment(x_train, y_train, x_test, y_test, model, confi
 
 
     if config.rank_correlation:
-        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #p_s(u_a, U_e)
+        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, -np.array(experiment_results.scores)) #p_s(u_a, U_e)
     else:
-        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #\rho(u_a, U_e)
+        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, -np.array(experiment_results.scores)) #\rho(u_e, U_e)
 
-    return config.term_weights[0] * np.abs(aleatoric_correlation - 0) + config.term_weights[1] * np.abs(epistemic_correlation - 1), experiment_results
+    return (aleatoric_correlation, epistemic_correlation), experiment_results
 
 def create_subsampled_dataset(x_train, y_train, dataset_size):
     X_train_subs = []
@@ -106,11 +106,10 @@ def decreasing_dataset_experiment_torch(train_dataset, val_dataset, model, confi
             experiment_results.epistemics.append(0.5)
 
     if config.rank_correlation:
-        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #p_s(u_a, U_e)
+        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, -np.array(experiment_results.scores)) #p_s(u_a, U_e)
     else:
-        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #\rho(u_a, U_e)
+        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, -np.array(experiment_results.scores)) #\rho(u_e, U_e)
 
-    return config.term_weights[0] * np.abs(aleatoric_correlation - 0) + config.term_weights[1] * np.abs(epistemic_correlation - 1), experiment_results
-
+    return (aleatoric_correlation, epistemic_correlation), experiment_results

disentanglement_error/error_metric.py
@@ -9,43 +9,80 @@ from disentanglement_error.util import Config, RunResults, CustomJsonEncoder
 
 
 def calculate_disentanglement_error(x_train, y_train, disentangling_model, x_test=None, y_test=None, kw_config=None, return_json=True):
-    config = Config(**kw_config)
+    if not kw_config:
+        config = Config()
+    else:
+        config = Config(**kw_config)
 
     if x_test is None or y_test is None:
         x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2)
 
-    disentanglement_errors = []
+    ua_Ue_corrs = np.empty(config.n_runs)
+    ue_Ue_corrs = np.empty(config.n_runs)
+    ua_Ua_corrs = np.empty(config.n_runs)
+    ue_Ua_corrs = np.empty(config.n_runs)
     results = RunResults()
     for run in range(config.n_runs):
-        decreasing_dataset_error, decreasing_dataset_result  = decreasing_dataset_experiment(x_train, y_train, x_test, y_test, disentangling_model, config)
-        label_noise_error, label_noise_result = label_noise_experiment(x_train, y_train, x_test, y_test, disentangling_model, config)
+        (corr_ua_Ue, corr_ue_Ue), decreasing_dataset_result  = decreasing_dataset_experiment(x_train, y_train, x_test, y_test, disentangling_model, config)
+        (corr_ua_Ua, corr_ue_Ua),  label_noise_result = label_noise_experiment(x_train, y_train, x_test, y_test, disentangling_model, config)
         results.label_noise_results.append(label_noise_result)
         results.decreasing_dataset_results.append(decreasing_dataset_result)
-        disentanglement_errors.append((decreasing_dataset_error + label_noise_error) / 4)
+        ua_Ua_corrs[run] = corr_ua_Ua
+        ua_Ue_corrs[run] = corr_ua_Ue
+        ue_Ua_corrs[run] = corr_ue_Ua
+        ue_Ue_corrs[run] = corr_ue_Ue
+
+    disentanglement_error = 1 / (1 + np.sum(config.term_weights)) * (
+            np.abs(ua_Ua_corrs - 1).mean() +
+            config.term_weights[0] * np.abs(ua_Ua_corrs - 1).mean() +
+            config.term_weights[1] * np.abs(ua_Ue_corrs).mean() +
+            config.term_weights[2] * np.abs(ue_Ua_corrs).mean()
+    )
 
     if return_json:
         results_json = json.dumps(results, cls=CustomJsonEncoder)
         config_json = json.dumps(config, cls=CustomJsonEncoder)
-        return np.sum(disentanglement_errors) / (1+ np.sum(config.term_weights)), results_json, config_json
+        return disentanglement_error, results_json, config_json
     else:
-        return np.sum(disentanglement_errors) / (1+ np.sum(config.term_weights))
+        return disentanglement_error, {"ua_Ua_corr": ua_Ua_corrs.mean(),
+                                       "ue_Ue_corr": ue_Ue_corrs.mean(),
+                                       "ua_Ue_corr": ua_Ue_corrs.mean(),
+                                       "ue_Ua_corr": ue_Ua_corrs.mean(),
+                                       }
 
 
 def calculate_disentanglement_error_torch(train_dataset, val_dataset, disentangling_model, batch_size, num_workers, kw_config=None, return_json=True):
-    config = Config(**kw_config)
+    if not kw_config:
+        config = Config()
+    else:
+        config = Config(**kw_config)
 
-    disentanglement_errors = []
+    ua_Ue_corrs = np.empty(config.n_runs)
+    ue_Ue_corrs = np.empty(config.n_runs)
+    ua_Ua_corrs = np.empty(config.n_runs)
+    ue_Ua_corrs = np.empty(config.n_runs)
+
     results = RunResults()
     for run in range(config.n_runs):
-        decreasing_dataset_error, decreasing_dataset_result  = decreasing_dataset_experiment_torch(train_dataset, val_dataset, disentangling_model, config, batch_size, num_workers)
-        label_noise_error, label_noise_result = label_noise_experiment_torch(train_dataset, val_dataset, disentangling_model, config, batch_size, num_workers)
+        (corr_ua_Ue, corr_ue_Ue), decreasing_dataset_result  = decreasing_dataset_experiment_torch(train_dataset, val_dataset, disentangling_model, config, batch_size, num_workers)
+        (corr_ua_Ua, corr_ue_Ua), label_noise_result = label_noise_experiment_torch(train_dataset, val_dataset, disentangling_model, config, batch_size, num_workers)
         results.label_noise_results.append(label_noise_result)
         results.decreasing_dataset_results.append(decreasing_dataset_result)
-        disentanglement_errors.append((decreasing_dataset_error + label_noise_error) / 4)
+        ua_Ua_corrs[run] = corr_ua_Ua
+        ua_Ue_corrs[run] = corr_ua_Ue
+        ue_Ua_corrs[run] = corr_ue_Ua
+        ue_Ue_corrs[run] = corr_ue_Ue
+
+    disentanglement_error = 1 / (1 + np.sum(config.term_weights)) * (
+            np.abs(ua_Ua_corrs - 1).mean() +
+            config.term_weights[0] * np.abs(ua_Ua_corrs - 1).mean() +
+            config.term_weights[1] * np.abs(ua_Ue_corrs).mean() +
+            config.term_weights[2] * np.abs(ue_Ua_corrs).mean()
+    )
 
     if return_json:
         results_json = json.dumps(results, cls=CustomJsonEncoder)
         config_json = json.dumps(config, cls=CustomJsonEncoder)
-        return np.sum(disentanglement_errors) / (1+ np.sum(config.term_weights)), results_json, config_json, results_json, config_json
+        return disentanglement_error, results_json, config_json
     else:
-        return np.sum(disentanglement_errors) / (1+ np.sum(config.term_weights))
+        return disentanglement_error

disentanglement_error/label_noise.py
@@ -26,13 +26,13 @@ def label_noise_experiment(x_train, y_train, x_test, y_test, model, config):
         experiment_results.epistemics.append(np.mean(epistemics))
 
     if config.rank_correlation:
-        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #p_s(u_a, U_a)
+        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, -np.array(experiment_results.scores)) #p_s(u_a, U_a)
     else:
-        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #\rho(u_a, U_a)
+        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, -np.array(experiment_results.scores)) #\rho(u_e, U_a)
 
-    return config.term_weights[0] * np.abs(aleatoric_correlation - 0) + config.term_weights[1] * np.abs(epistemic_correlation - 1), experiment_results
+    return (aleatoric_correlation, epistemic_correlation), experiment_results
 
 
 def partial_shuffle_dataset(x, y, percentage):
@@ -77,17 +77,16 @@ def label_noise_experiment_torch(train_dataset, val_dataset, model, config, batc
             experiment_results.epistemics.append(0.5)
 
     if config.rank_correlation:
-        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = spearmanr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #p_s(u_a, U_a)
+        epistemic_correlation, _ = spearmanr(experiment_results.epistemics, -np.array(experiment_results.scores)) #p_s(u_a, U_a)
     else:
-        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, experiment_results.scores)
-        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, experiment_results.scores)
+        aleatoric_correlation, _ = pearsonr(experiment_results.aleatorics, -np.array(experiment_results.scores)) #\rho(u_a, U_a)
+        epistemic_correlation, _ = pearsonr(experiment_results.epistemics, -np.array(experiment_results.scores)) #\rho(u_e, U_a)
 
-
-    return 1 * np.abs(aleatoric_correlation - 1) + config.term_weights[2] * np.abs(epistemic_correlation - 0), experiment_results
-
+    return (aleatoric_correlation, epistemic_correlation), experiment_results
 
 def create_partial_shuffle_dataloader_torch(dataset, percentage, batch_size, workers):
+    dataset = dataset.copy() # We shuffle in place, this prevents the original dataset from being modified
     indices = np.random.choice(np.arange(len(dataset.samples)), size=int(percentage * len(dataset.samples)), replace=False)
 
     subset_to_shuffle = [dataset.samples[index] for index in indices]